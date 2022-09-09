bfd2b4ac05e195282a641442ec6bafbd555e661f
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2022-09-09 11:02:30 +0200

    error 404

---
+++ versie-3/scripts/script.js
@@ -0,0 +1,9 @@
+const expandButton = document.querySelector('header button')
+expandButton.addEventListener('click', expand)
+
+function expand () {
+  document.body.classList.toggle('expand')
+  
+}
+
+
